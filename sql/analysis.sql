55bcac4022cced27a498964f00dd097d7f78c11c
Jeanne Ferrer <52438473+jeanneferrer@users.noreply.github.com>
2026-02-28 21:40:40 -0700

    Update analysis.sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,13 +1,20 @@
 -- Applications per platform 
-SELECT platform AS "How I Applied", COUNT(*) AS "Number of Applications"
+SELECT 
+	platform AS "How I Applied",
+	COUNT(*) AS "Number of Applications", 
+	SUM(CASE WHEN is_tech == "Tech" THEN 1 ELSE 0 END) AS "Tech",
+	SUM(CASE WHEN is_tech == "Non-Tech" THEN 1 ELSE 0 END) AS "Non-Tech"
 	FROM my_applications
-	GROUP BY platform; -- Note: add how many tech and non tech?
+	GROUP BY platform;  -- add  status?
 
 -- Tech vs non-tech counts
 SELECT is_tech AS "Tech or Non-Tech", COUNT(*) AS "Number of Applications"
 	FROM my_applications
 	GROUP BY is_tech;
 	
--- Response Rate 
+-- Response Rate = use CASE for 
+SELECT status, COUNT(*) AS "Number of Applications"
+	FROM my_applications
+	GROUP BY status;
 
 -- Interview Rate
